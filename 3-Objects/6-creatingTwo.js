2bf18512bc9ff160648c1c990a49440fed000d0e
Miłosz Topolewski <topolewski.webmilo@gmail.com>
2022-05-10 08:30:11 +0200

    objects2

---
+++ 3-Objects/6-creatingTwo.js
@@ -0,0 +1,12 @@
+// Write a function that takes two strings (a and b) as arguments
+// Create an object that has a property with key 'a' and a value of 'b'
+// Return the object
+function myFunction(a, b) {
+  return { [a]: b };
+}
+
+function myFunction2(a, b) {
+  let result = {};
+  result[a] = b;
+  return result;
+}
--- 3-Objects/7-creatingThree.js
+++ 3-Objects/7-creatingThree.js
@@ -1,12 +1,15 @@
 // Write a function that takes two arrays (a and b) as arguments
 // Create an object that has properties with keys 'a' and corresponding values 'b'
 // Return the object
+
 function myFunction(a, b) {
+  return a.reduce((acc, cur, i) => ({ ...acc, [cur]: b[i] }), {});
+}
+
+function myFunction2(a, b) {
   let result = {};
   a.forEach((el, i) => {
     result[el] = b[i];
   });
-  console.log(result);
+  return result;
 }
-
-myFunction(['w', 'x', 'y', 'z'], [10, 9, 5, 2]);
