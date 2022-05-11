f45180bcf4037e464345d5e071d8862a890329d9
Miłosz Topolewski <topolewski.webmilo@gmail.com>
2022-05-11 18:30:12 +0200

    objects3

---
+++ 3-Objects/9-sum.js
@@ -0,0 +1,5 @@
+// Write a function that takes an object (a) as argument
+// Return the sum of all object values
+function myFunction(a) {
+  return Object.values(a).reduce((acc, curr) => acc + curr);
+}
